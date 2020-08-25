8bb295e2b3ca25b636aeb547dac798feac634a7a
DESKTOP-B4SLFDV\ymstm <yo.stm@outlook.com>
2020-08-25 14:38:51 +0200

    mise à jour ServletRegister (methode doGet)

--- src/fr/eni/projet/servlets/ServletRegister.java
+++ src/fr/eni/projet/servlets/ServletRegister.java
@@ -2,6 +2,7 @@ package fr.eni.projet.servlets;
 
 import java.io.IOException;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -23,56 +24,10 @@ public class ServletRegister extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// Récupération des données saisies, envoyés en tant que paramètres
-		// de la requête GET générée à la validation du formaulaire
-		String pseudo = request.getParameter("#");
-		String nom = request.getParameter("#");
-		String prenom = request.getParameter("#");
-		String email = request.getParameter("#");
-		String telephone = request.getParameter("#");
-		String rue = request.getParameter("#");
-		String codePostal = request.getParameter("#");
-		String ville = request.getParameter("#");
-		String mdp = request.getParameter("#");
-		String confirmMdp = request.getParameter("#");
 		
-		String message;
+		RequestDispatcher rd = request.getRequestDispatcher("WEB_INF/jsp/register.jsp");
+		rd.forward(request, response);
 		
-		/*
-		 * Initialisation de message à afficher :
-		 * Si un des champs obligatoires du formulaire n'est pas renseigné,
-		 * alors on affiche un message d'erreur
-		 * Sinon on affiche un message de succès
-		 * 
-		 */
-		if (nom.trim().isEmpty() || rue.trim().isEmpty() || ville.trim().isEmpty() || telephone.trim().isEmpty()) {
-			message = "Attention - Tous les champs obligatoires doivent être remplis.";
-			response.sendRedirect("http://localhost:8080/ProjetEnchere/Register");
-			
-		} else if (mdp.trim() != confirmMdp.trim()){
-			// Ici on test si le champ confirmer mot passe est différent du mot de passe souhaité.
-			message = "Attention - Tous les champs obligatoires doivent être remplis.";
-			response.sendRedirect("http://localhost:8080/ProjetEnchere/Register");
-		} else {
-			message = "Client crééavec succès !";
-		}
-			
-		/*
-		 * Création du bean Client et initialisation avec les données récupérées
-		 */
-		Utilisateurs user = new Utilisateurs(pseudo, nom, prenom, email, telephone, rue, codePostal, mdp, 0);
-		
-		/*
-		 * Ajout du bean et du message à l'objet requête
-		 */
-		request.setAttribute("user", user);
-		request.setAttribute("message", message);
-		
-		/*
-		 * Transmission à la pageJSP en charge de l'affichage des données.
-		 */
-		this.getServletContext().getRequestDispatcher("/userProfile").forward(request, response);
-		//
 		
 	}
 
@@ -80,8 +35,57 @@ public class ServletRegister extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+		
+		// Récupération des données saisies, envoyés en tant que paramètres
+				// de la requête GET générée à la validation du formaulaire
+				String pseudo = request.getParameter("#");
+				String nom = request.getParameter("#");
+				String prenom = request.getParameter("#");
+				String email = request.getParameter("#");
+				String telephone = request.getParameter("#");
+				String rue = request.getParameter("#");
+				String codePostal = request.getParameter("#");
+				String ville = request.getParameter("#");
+				String mdp = request.getParameter("#");
+				String confirmMdp = request.getParameter("#");
+				
+				String message;
+				
+				/*
+				 * Initialisation de message à afficher :
+				 * Si un des champs obligatoires du formulaire n'est pas renseigné,
+				 * alors on affiche un message d'erreur
+				 * Sinon on affiche un message de succès
+				 * 
+				 */
+				if (nom.trim().isEmpty() || rue.trim().isEmpty() || ville.trim().isEmpty() || telephone.trim().isEmpty()) {
+					message = "Attention - Tous les champs obligatoires doivent être remplis.";
+					response.sendRedirect("http://localhost:8080/ProjetEnchere/Register");
+					
+				} else if (mdp.trim() != confirmMdp.trim()){
+					// Ici on test si le champ confirmer mot passe est différent du mot de passe souhaité.
+					message = "Attention - Tous les champs obligatoires doivent être remplis.";
+					response.sendRedirect("http://localhost:8080/ProjetEnchere/Register");
+				} else {
+					message = "Client crééavec succès !";
+				}
+					
+				/*
+				 * Création du bean Client et initialisation avec les données récupérées
+				 */
+				Utilisateurs user = new Utilisateurs(pseudo, nom, prenom, email, telephone, rue, codePostal, mdp, 0);
+				
+				/*
+				 * Ajout du bean et du message à l'objet requête
+				 */
+				request.setAttribute("user", user);
+				request.setAttribute("message", message);
+				
+				/*
+				 * Transmission à la pageJSP en charge de l'affichage des données.
+				 */
+				this.getServletContext().getRequestDispatcher("/userProfile").forward(request, response);
+				//
 	}
 
 }
