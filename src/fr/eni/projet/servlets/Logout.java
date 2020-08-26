88e65d81a127da254ad23d83cf8ed801a00bc64b
DESKTOP-B4SLFDV\ymstm <yo.stm@outlook.com>
2020-08-26 14:35:08 +0200

    nouvelle modif Servlet logout

--- src/fr/eni/projet/servlets/Logout.java
+++ src/fr/eni/projet/servlets/Logout.java
@@ -25,7 +25,7 @@ public class Logout extends HttpServlet {
 		HttpSession session = request.getSession();
 		session.invalidate();
 		
-		response.sendRedirect("http://localhost:8080/ProjetEnchere/Login");
+		response.sendRedirect("http://localhost:8080/ProjetEnchere/Index");
 	}
 
 	/**
