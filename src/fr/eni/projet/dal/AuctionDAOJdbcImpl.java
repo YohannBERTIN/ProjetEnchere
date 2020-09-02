4c6957a13f127a4cad35c92e130dd6fc9fb96872
DESKTOP-B4SLFDV\ymstm <yo.stm@outlook.com>
2020-09-02 11:06:04 +0200

    Suppression de parties inutiles dans AuctionDAOJdbcImpl

--- src/fr/eni/projet/dal/AuctionDAOJdbcImpl.java
+++ src/fr/eni/projet/dal/AuctionDAOJdbcImpl.java
@@ -32,64 +32,12 @@ public class AuctionDAOJdbcImpl implements AuctionDAO {
 		
 		@Override
 		public void insert(Item soldItem, Auctions auction, Pickup pickup) throws BusinessException {
-			if(soldItem==null || auction==null || pickup==null) {
-				BusinessException businessException = new BusinessException();
-				businessException.ajouterErreur(CodesResultatDAL.INSERT_OBJET_NULL);
-				throw businessException;
-			}
-			
-			try(Connection cnx = ConnectionProvider.getConnection()) {
-				
-				try {
-					cnx.setAutoCommit(false);
-					
-					PreparedStatement pstmt = cnx.prepareStatement(INSERT_AUCTION, PreparedStatement.RETURN_GENERATED_KEYS);
-					pstmt.setString(1, soldItem.getNomArticle());
-					pstmt.setString(2, soldItem.getDescription());
-					pstmt.setString(3, soldItem.getDateDebutEnchere().toString());
-					pstmt.setString(3, soldItem.getDateFinEnchere().toString());
-					pstmt.setInt(5, soldItem.getPrixInitial());
-					pstmt.setString(6, soldItem.getNoUtilisateur().toString());
-					pstmt.setString(7, soldItem.getNoCategorie().toString());
-					
-					pstmt.setString(8, pickup.getRue());
-					pstmt.setString(9, pickup.getCodePostal());
-					pstmt.setString(10, pickup.getVille());
-					
-					pstmt.setString(11, auction.getDateEnchere().toString());
-					pstmt.setInt(12, auction.getMontantEnchere());
-					
-					pstmt.executeUpdate();
-					
-					ResultSet rs = pstmt.getGeneratedKeys();
-					if(rs.next()) {
-						soldItem.setNoArticle(rs.getInt(1));
-					}
-					
-					rs.close();
-					pstmt.close();
-					
-					cnx.commit();
-					
-					
-					
-				} catch(Exception e) {
-					e.printStackTrace();
-					cnx.rollback();
-					throw e;
-				}
-				
-			} catch (Exception e) {
-				e.printStackTrace();
-				BusinessException businessException = new BusinessException();
-				businessException.ajouterErreur(CodesResultatDAL.INSERT_OBJET_ECHEC);
-				throw businessException;
-			}
+		
 		}
 
 		@Override
 		public void delete(Auctions auction) throws BusinessException {
-			// TODO Auto-generated method stub
+		
 			
 		}
 }
